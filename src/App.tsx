309151c015de7dacd88b787a78d7fb8b1a84179f
RichCode <stanleyogada2000@gmail.com>
2023-06-09 18:02:37 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,58 @@
+import { SliderThemeProvider } from "react-rc-carousel";
+import Hero from "./Hero";
+import Products from "./Products";
+
+function App() {
+  return (
+    <>
+      jshkjashdkja
+      <Hero />
+      <br />
+      <br />
+      <Hero />
+      <br />
+      <br />
+      <SliderThemeProvider
+        props={{
+          nSlidePerView: 1,
+          // isShowDots: false,
+          breakpoints: [
+            { width: 391, nSlidePerView: 1 },
+            { width: 592, nSlidePerView: 2 },
+            { width: 830, nSlidePerView: 3 },
+            { width: 920, nSlidePerView: 4 },
+          ],
+          theme: {
+            backgroundColor: "#ffaaaa",
+            color: "red",
+          },
+        }}
+      >
+        <Products heading="Top Products" />
+        <Products heading="New Products" />
+        <Products heading="Hot Popular" />
+      </SliderThemeProvider>
+      <SliderThemeProvider
+        props={{
+          nSlidePerView: 2,
+          isAutoSlide: false,
+          breakpoints: [
+            { width: 391, nSlidePerView: 1 },
+            { width: 592, nSlidePerView: 2 },
+            { width: 830, nSlidePerView: 3 },
+            { width: 920, nSlidePerView: 4 },
+          ],
+        }}
+      >
+        <Products heading="Featured Products" />
+        <Products heading="Deals For June" />
+      </SliderThemeProvider>
+      <br />
+      <br />
+      <br />
+      <br />
+    </>
+  );
+}
+
+export default App;
